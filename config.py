b0176938cea9658eeea4057025daa6c47785db8c
Raymond Yuan <ray.yuan0@gmail.com>
2017-12-04 15:29:24 -0600

    pyshit

---
+++ config.py
@@ -0,0 +1,19 @@
+SEQ_LEN = 10
+BATCH_SIZE = 4
+LEFT_CONTEXT = 5
+
+# These are the input image parameters.
+HEIGHT = 480
+WIDTH = 640
+CHANNELS = 3 # RGB
+
+# The parameters of the LSTM that keeps the model state.
+RNN_SIZE = 32
+RNN_PROJ = 32
+
+NUM_EPOCHS = 100
+
+# Our training data follows the "interpolated.csv" format from Ross Wightman's scripts.
+CSV_HEADER = "index,timestamp,width,height,frame_id,filename,angle,torque,speed,lat,long,alt".split(",")
+OUTPUTS = CSV_HEADER[-6:-3] # angle,torque,speed
+OUTPUT_DIM = len(OUTPUTS) # predict all features: steering angle, torque and vehicle speed
